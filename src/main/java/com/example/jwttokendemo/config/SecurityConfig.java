0a28e5a6dca658d5ef5698b8d338c87d0b5cff4c
Seiya-mui <vanessa0702128@gmail.com>
2025-02-10 16:46:48 +0800

    update password encoded type

--- src/main/java/com/example/jwttokendemo/config/SecurityConfig.java
+++ src/main/java/com/example/jwttokendemo/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -45,8 +45,7 @@ public class SecurityConfig{
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 
 }
